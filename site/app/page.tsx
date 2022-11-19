b5591a76842916a39e0891a752a0f5e19cd38ad2
ntwali edson <ntwaliedson9@gmail.com>
2022-11-20 00:07:51 +0200

    feat:input created

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+"use client";
+
+import Input from "@components/ui/Input";
+import React, { useState } from "react";
 
 export default function Page() {
-  return <div>Hello</div>;
+  const [value, setvalue] = useState("");
+  return (
+    <div>
+      <Input
+        placeholder="First name"
+        onChange={setvalue}
+        label="Enter first name"
+        value={value}
+      />
+    </div>
+  );
 }
