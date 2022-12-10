70c06ca41cb949d41fa56040928e425abfe3b37f
Ntwali Edson <ntwaliedson9@gmail.com>
2022-12-11 01:22:38 +0200

    Implemented the radio,checkox and a switch

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,14 +1,39 @@
 "use client";
 
+import Switch from "@components/Switch";
 import Checkbox from "@components/ui/Checkbox";
 import Radio from "@components/ui/Radio";
 import React, { useState } from "react";
 
 export default function Page() {
-  const [c, setc] = useState(false);
+  const [checked, setchecked] = useState(false);
   return (
     <div>
-      <Radio />
+      <div className="m-5">
+        <Radio
+          checked={true}
+          onChange={() => {}}
+          label="How is the gym training"
+        />
+        <Radio label="What is andela" onChange={() => {}} />
+      </div>
+      <div className="m-5">
+        <Checkbox
+          checked={checked}
+          onChange={() => {
+            setchecked(!checked);
+          }}
+          label="Click me to change the selected"
+        />
+      </div>
+      <div className="my-10 mx-5">
+        <Switch
+          onChange={() => {}}
+          title="Available to hire"
+          subTitle="Nulla amet tempus sit accumsan. Aliquet turpis sed sit lacinia."
+        />
+        <Switch onChange={() => {}} label="Aliquet turpis sed sit lacinia." />
+      </div>
     </div>
   );
 }
--- site/components/ui/Checkbox.tsx
+++ site/components/ui/Checkbox.tsx
@@ -7,29 +7,26 @@ interface props {
   checked?: boolean;
 }
 
-export default function Checkbox({
-  label,
-  onChange,
-  checked = false,
-  id,
-}: props) {
+export default function Checkbox({ label, onChange, checked = false }: props) {
   return (
-    <div className="flex items-center">
+    <div className="flex my-2 items-center">
       <div>
         <input
-          className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-transparent checked:bg-primary checked:border-primary focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          className="form-check-input appearance-none h-4 w-4 border border-gray-500 rounded-sm bg-transparent checked:bg-primary checked:border-primary focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
           type="checkbox"
-          id={id}
+          id={label || "checkbox"}
           onChange={onChange}
           checked={checked}
         />
       </div>
-      <label
-        className="form-check-label font-normal leading-8 inline-block text-gray-100 cursor-pointer capitalize text-sm"
-        htmlFor={id}
-      >
-        {label}
-      </label>
+      {label && (
+        <label
+          htmlFor={label || "checkbox"}
+          className="text-sm cursor-pointer text-gray-200 flex ml-2"
+        >
+          {label}
+        </label>
+      )}
     </div>
   );
 }
