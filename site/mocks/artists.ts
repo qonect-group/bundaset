ddbf41e6ce393899b4a6bc5588daaa15457a6e16
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-01-24 23:47:42 +0200

    feat: add artists card

---
+++ site/mocks/artists.ts
@@ -0,0 +1,56 @@
+export const Artists:Array<{}> = [
+    {
+        id: 1,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    },
+    {
+        id: 2,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"  
+    },
+    {
+        id: 3,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    },
+    {
+        id: 4,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    },
+    {
+        id: 5,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    },
+    {
+        id: 6,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    },
+     {
+        id: 7,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    },
+    {
+        id: 8,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    },
+    {
+        id: 9,
+        name: "Ruth B",
+        type:"Adele",
+        image:"/assets/img/artist-1.png"
+    }
+]
